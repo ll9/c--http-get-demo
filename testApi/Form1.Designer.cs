134946fcb716cc8560b860bac12cc763e3b98456
ll9 <llwierer@web.de>
2018-10-23 22:38:52 +0200

    Getting basic epsg code per get request

--- testApi/Form1.Designer.cs
+++ testApi/Form1.Designer.cs
@@ -28,13 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.EpsgBox = new System.Windows.Forms.TextBox();
+            this.ResultBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(145, 17);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Get Request";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // EpsgBox
+            // 
+            this.EpsgBox.Location = new System.Drawing.Point(12, 17);
+            this.EpsgBox.Name = "EpsgBox";
+            this.EpsgBox.Size = new System.Drawing.Size(100, 20);
+            this.EpsgBox.TabIndex = 0;
+            // 
+            // ResultBox
+            // 
+            this.ResultBox.Location = new System.Drawing.Point(12, 76);
+            this.ResultBox.Multiline = true;
+            this.ResultBox.Name = "ResultBox";
+            this.ResultBox.Size = new System.Drawing.Size(208, 42);
+            this.ResultBox.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(236, 152);
+            this.Controls.Add(this.ResultBox);
+            this.Controls.Add(this.EpsgBox);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox EpsgBox;
+        private System.Windows.Forms.TextBox ResultBox;
     }
 }
 
--- testApi/Form1.cs
+++ testApi/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +16,18 @@ namespace testApi
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            var epsgCode = EpsgBox.Text;
+            var url = $"http://spatialreference.org/ref/epsg/{epsgCode}/proj4/";
+
+            var client = new HttpClient();
+            var resp = await client.GetAsync(url);
+            var txt = await resp.Content.ReadAsStringAsync();
+
+            ResultBox.Text = txt;
         }
     }
 }
